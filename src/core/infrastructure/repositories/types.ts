7ee086a3e6c850f67e9b5e0b6f76fb5c49c71623
zavvdev <zaviryukha.igor99@gmail.com>
2025-12-09 13:48:13 +0000

    ref

--- src/core/infrastructure/repositories/types.ts
+++ src/core/infrastructure/repositories/types.ts
@@ -1,16 +1,12 @@
 import { z as t, ZodType } from "zod";
 
-// If your server returns a specific response format, you can create a reusable schema for it here
-// and use it across your repository response schemas.
 export const createResponseSchema = <T extends ZodType>(data: T) =>
   t.object({
-    // ex:
-    // status: t.string(),
-    // message: t.string(),
+    status: t.string(),
+    message: t.string(),
     data,
   });
 
-// and if you have a response that you don't care of the data shape
 export type AnyResponse = t.infer<
   ReturnType<typeof createResponseSchema<t.ZodTypeAny>>
 >;
